d68e73c911b8a1f5ec8bb15f03659a8147a59c70
Pasindu Ukwatta <pasindu.reccelabs@gmail.com>
2020-09-26 17:21:14 +0530

    Intial Commit Inital Commit

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {BrowserRouter,Route,Link} from 'react-router-dom'
+
+const PageOne =() =>{
+    return (
+        <div>
+            Page 1
+            <br />
+            <Link to='/pagetwo'>Naviagete to Page 2</Link>
+
+        </div>
+    )
+}
+
+const PageTwo =() =>{
+    return (<div>
+        Page 2
+        <br />
+        <button>Click Me !</button>
+        <br />
+        <Link to='/'>Naviagete to Page 1</Link>
+     </div>)
+}
+
+const App =()=>{
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path ="/" exact component ={PageOne}/>
+                    <Route path ="/pagetwo"  component ={PageTwo}/>
+                </div>
+
+            </BrowserRouter>
+
+        </div>
+    )
+}
+
+export default App
